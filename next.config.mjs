e974ebd2b98823879d8230d544fda9e498951d0c
m1s5 <m1s5.trash@yandex.ru>
2024-02-14 16:59:37 +0300

    a

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
   // skipTrailingSlashRedirect: true,
 
   // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  distDir: "out",
 };
 
 export default nextConfig;
